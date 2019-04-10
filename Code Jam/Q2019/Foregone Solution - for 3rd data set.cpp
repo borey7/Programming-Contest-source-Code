51430416e8af944a6f0c6e29cbe684ba7581ccf2
borey <skborey@gmail.com>
2019-04-10 08:12:33 +0700

    add CodeJam2019-problem1 passed all data test

---
+++ Code Jam/Q2019/Foregone Solution - for 3rd data set.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void getOnePair(string n) {
+	
+	string ans = "", a = "", b = "";
+	int l = n.size();
+	
+	for (int i=0; i<l; ++i) {
+		if (n[i] == '4') {
+			a.push_back('3');
+			b.push_back('1');
+		} else {
+			a.push_back(n[i]);
+			if (!b.empty()) {
+				b.push_back('0');	
+			}
+		}
+	}
+
+	cout << a << " " << (b.empty() ? "0" : b) << endl;
+}
+
+int main() {
+	
+	int t;
+	string n;
+	
+	cin >> t;
+	getchar();
+	
+	for (int i=1; i<=t; ++i) {
+		
+		getline(cin, n);
+		
+		cout << "Case #" << i << ": ";
+		getOnePair(n);
+	}
+	
+	return 0;
+}
