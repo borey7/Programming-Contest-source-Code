e7a4d853acb9c276bfea5529383a0b02461fdccf
Borey Sok <skborey@gmail.com>
2020-04-05 00:41:53 +0700

    codejam q 20 - solution 3 - bruteforce algorithm - time limit for n <= 1000

--- code_jam/Q2020/3.py
+++ code_jam/Q2020/3.py
@@ -1,22 +1,37 @@
 t = int(input())
 for case in range(t):
     result = ''
-    cam_free = [True]*(24*60+1) #Cameron's time slot 0 -> 1440 is free
-    jam_free = [True]*(24*60+1) #Jamie's time slot 0 -> 1440 is free
+    # cam_free = [True]*(24*60+1) #Cameron's time slot 0 -> 1440 is free
+    # jam_free = [True]*(24*60+1) #Jamie's time slot 0 -> 1440 is free
+    
+    n = int(input())
+    acts = [ list(map(int, input().split())) for i in range(n) ]
 
-    for i in range(int(input())):
-        s, e = map(int, input().split())
-        
-        if not result == 'IMPOSSIBLE':    
-            if all(jam_free[s:e]): #assign to Cameron
+    for i in range(pow(2, n)): #[CCCC, CCCJ, CCJC, ...]
+        parents = [[True]*(24*60+1), [True]*(24*60+1)] #Cameron = 0, Jamie = 1
+        possible = bin(i)[2:].zfill(n) # CCCC
+        hahahaha = 0
+        for index in range(n): # verify possible schedul with parents' time slot
+            
+            # start for first activit to the last
+            s, e = acts[index]
+            c_or_j = int(possible[index])
+            if all(parents[c_or_j][s:e]): #if that slot really free
+                #update time slot to not free
                 for minute in range(s, e):
-                    jam_free[minute] = False
-                result += 'J'
-            elif all(cam_free[s:e]): #assign to Jamie
-                for minute in range(s, e):
-                    cam_free[minute] = False
-                result += 'C'
-            else:
-                result = 'IMPOSSIBLE'
+                    parents[c_or_j][minute] = False
+                hahahaha += 1
+            else: #if not, no need to check next activit in this possible
+                continue #the CCCC is impossible, continue to next CCCJ, ...
+
+        if hahahaha == n: #found a possible case, stop check next CCCJ, go to next use case
+            result = possible
+            break
+    
+    if result == '': # cannot find possible case
+        result = 'IMPOSSIBLE'
+    else:
+        result = result.replace('0', 'C')
+        result = result.replace('1', 'J')
 
     print('Case #%d: %s'%(case+1, result))
