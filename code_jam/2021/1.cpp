310d39d76f4eb7000c1817a9af1d2b4045dc38e8
skborey <skborey@gmail.com>
2021-03-27 16:43:04 +0700

    accept :fire: solution for code jam 2021 problem 1

--- code_jam/2021/1.cpp
+++ code_jam/2021/1.cpp
@@ -20,29 +20,42 @@ int getJ(int list[], int n, int i)
 int getCost(int list[], int n)
 {
     int cost = 0;
-    int newList[n], previousList[n];
+    int newList[n];
     for (int idx = 0; idx < n; idx++)
     {
         newList[idx] = list[idx];
-        previousList[idx] = list[idx];
     }
-
+    // cout << "----------- new list > ";
+    // for (int idx = 0; idx < n; idx++)
+    // {
+    //     cout << newList[idx] << " ";
+    // }
     for (int i = 0; i < n - 1; i++)
     {
         int j = getJ(newList, n, i);
 
         // reverse from index of i to j
-        if (i != j)
+        // cout << "\n=== reverse " << i << " to " << j << endl;
+        for (int idx = 0; idx <= (j - i) / 2; idx++)
         {
-            for (int idx = i; idx <= j; idx++)
-            {
-                newList[j - idx] = previousList[idx];
-            }
-            for (int idx = 0; idx < n; idx++)
-            {
-                previousList[idx] = newList[n];
-            }
+            int temp = newList[i + idx];
+            newList[i + idx] = newList[j - idx];
+            newList[j - idx] = temp;
+            // cout << (i + idx) << " and " << (j - idx) << " : " << newList[i + idx] << " < - > " << newList[j - idx] << endl;
+            // cout << "          result: ";
+            // for (int aa = 0; aa < n; aa++)
+            // {
+            //     cout << newList[aa] << " ";
+            // }
+            // cout << endl;
         }
+
+        // cout << "\n-----------> ";
+        // for (int idx = 0; idx < n; idx++)
+        // {
+        //     cout << newList[idx] << " ";
+        // }
+        // cout << endl;
 
         cost += (j + 1) - (i + 1) + 1;
     }
