c2da133576ab4fab2df9ad11d97b466cb0dfe5d4
skborey <skborey@gmail.com>
2025-01-10 23:39:27 +0700

    Add word to number - 100 case

--- free-time/001_word_to_number.js
+++ free-time/001_word_to_number.js
@@ -1,4 +1,4 @@
-const dic = {
+const oneDigit = {
   zero: 0,
   one: 1,
   two: 2,
@@ -9,10 +9,30 @@ const dic = {
   seven: 7,
   eight: 8,
   nine: 9,
+  ten: 10,
+  eleven: 11,
+  twelve: 12,
+  twenty: 20,
 };
 
+const base = {
+  hundred: 100,
+};
+
+function cal(w) {
+  const digits = w.split(" ");
+
+  return digits.reduce((result, d) => {
+    if (oneDigit[d]) {
+      return result + oneDigit[d];
+    } else {
+      return result * base[d];
+    }
+  }, 0);
+}
+
 function translate(w) {
-  return dic[w];
+  return oneDigit[w] !== undefined ? oneDigit[w] : cal(w);
 }
 
 module.exports = translate;
--- free-time/001_word_to_number.test.js
+++ free-time/001_word_to_number.test.js
@@ -11,6 +11,33 @@ test.each([
   ["seven", 7],
   ["eight", 8],
   ["nine", 9],
-])("one digit word - %s", (w, n) => {
+  ["ten", 10],
+  ["eleven", 11],
+  ["twelve", 12],
+])("One digit word - %s: %s", (w, n) => {
+  expect(translate(w)).toBe(n);
+});
+
+test.each([
+  ["twenty one", 21],
+  ["twenty two", 22],
+])("Two digits word - %s: %s", (w, n) => {
+  expect(translate(w)).toBe(n);
+});
+
+test.each([
+  ["one hundred one", 101],
+  ["one hundred two", 102],
+  ["one hundred twenty", 120],
+  ["two hundred two", 202],
+  ["four hundred eleven", 411],
+])("Three digits word - %s: %s", (w, n) => {
+  expect(translate(w)).toBe(n);
+});
+
+test.each([
+  ["three hundred twenty one", 321],
+  ["five hundred twenty one", 521],
+])("Four words - %s: %s", (w, n) => {
   expect(translate(w)).toBe(n);
 });
